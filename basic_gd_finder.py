39a9cd070ea036c0707d0d38ab10b7e1b4a87acc
seedcrack <j5tanabodee@gmail.com>
2025-01-07 22:53:19 +0700

    Update basic_gd_finder.py

--- basic_gd_finder.py
+++ basic_gd_finder.py
@@ -11,8 +11,10 @@ with open("unchecked.txt", 'r') as file:
     search_prompt = ast.literal_eval(file_content)
     
 for k,v in search_prompt.items():
-    if (api.user(v)).previous_usernames or k.endswith('_old'): # skip those with previous username for now bc it's an additional headache
+    if (api.user(v)).previous_usernames: # skip those with previous username for now bc it's an additional headache
         continue
+    if k.endswith('_old'):
+        k = k.replace('_old','')
     result = (api.search_beatmapsets(query=k).total)
     user = api.user(v)
     compare = user.ranked_and_approved_beatmapset_count + user.loved_beatmapset_count + user.guest_beatmapset_count
